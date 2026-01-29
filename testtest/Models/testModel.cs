7106beb0d803f61f3ac3162e63965a895c94a02e
TeawZY <teawziyi@maxsys.com.my>
2026-01-29 08:37:08 +0800

    NEWNEW

--- testtest/Models/testModel.cs
+++ testtest/Models/testModel.cs
@@ -3,6 +3,7 @@
     public class testModel
     {
         public int Id { get; set; }
-        public string Name { get; set; }
+        public string Name { get; set; } = "Test";
+        public string Description { get; set; } = "Test";
     }
 }
