4d5cda84335b52bc3df1f79f1196af941a807b79
TeawZY <teawziyi@maxsys.com.my>
2026-01-29 08:54:11 +0800

    msg

--- testtest/Models/testModel.cs
+++ testtest/Models/testModel.cs
@@ -5,5 +5,6 @@
         public int Id { get; set; }
         public string Name { get; set; } = "Test";
         public string Description { get; set; } = "Test";
+        public string title { get; set; } = "Test";
     }
 }
