4752fb5e41ac892815f7e7bcf659e8e31464afb1
TeawZY <teawziyi@maxsys.com.my>
2026-01-30 12:19:37 +0800

    helo, this is the test for the revert

--- testtest/Models/testModel.cs
+++ testtest/Models/testModel.cs
@@ -3,7 +3,7 @@
     public class testModel
     {
         public int Id { get; set; }
-        public string Name { get; set; } = "Test";
+        public string Name { get; set; } = "Test12345";
         public string Description { get; set; } = "Test";
         public string title { get; set; } = "Test1";
         public string description { get; set; } = "Test for the cli1";
